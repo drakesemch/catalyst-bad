bf26bde2849e286f913aa9ee031e1b0e1b1a38f6
Drake Semchyshyn <desemchyshyn@gmail.com>
2024-03-18 17:56:07 -0500

    Added analytics

--- src/components/misc/providers.tsx
+++ src/components/misc/providers.tsx
@@ -5,22 +5,25 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 import { TooltipProvider } from "../ui/tooltip";
 import { ClerkProvider, MultisessionAppSupport } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
 import { env } from "@/env";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider publishableKey={env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-      <MultisessionAppSupport>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <TooltipProvider>{children}</TooltipProvider>
-        </ThemeProvider>
-      </MultisessionAppSupport>
-    </ClerkProvider>
+    <Analytics>
+      <ClerkProvider publishableKey={env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+        <MultisessionAppSupport>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <TooltipProvider>{children}</TooltipProvider>
+          </ThemeProvider>
+        </MultisessionAppSupport>
+      </ClerkProvider>
+    </Analytics>
   );
 }
 
