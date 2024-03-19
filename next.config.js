7fa0055ed56bd2d1505d3d8aad70947aef1796eb
Drake Semchyshyn <desemchyshyn@gmail.com>
2024-03-18 20:21:51 -0500

    Fixed Next Config

--- next.config.js
+++ next.config.js
@@ -3,15 +3,7 @@ await import("./src/env.js");
 import { withSentryConfig } from "@sentry/nextjs";
 
 export default withSentryConfig(
-  {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    experimental: {
-      serverActions: {
-        allowedOrigins: ["141h4rk5-3000.use2.devtunnels.ms", "localhost:3000"],
-      },
-    },
-  },
+  {},
   {
     silent: true,
     org: "blue-flame-tb",
