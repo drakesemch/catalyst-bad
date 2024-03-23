708a0f26a36a0135f981f08727a74170c1a0bb67
Drake Semchyshyn <desemchyshyn@gmail.com>
2024-03-23 01:37:12 -0500

    Moved from drizzle to prisma

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
-import { postRouter } from "~/server/api/routers/post";
+import { postRouter } from "~/server/api/routers/ex";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { authRouter } from "./routers/auth";
+import { canvasRouter } from "./routers/canvas";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +9,8 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  auth: authRouter,
+  canvas: canvasRouter,
 });
 
 // export type definition of API
